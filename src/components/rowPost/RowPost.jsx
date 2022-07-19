dab7030452b3b43a7b041e58e98b25fbd77a8a98
kiran-oo7 <kiran.antony02@email.com>
2022-07-19 10:47:43 +0530

    Youtube added

--- src/components/rowPost/RowPost.jsx
+++ src/components/rowPost/RowPost.jsx
@@ -1,19 +1,36 @@
 import "./rowPost.css"
-import { Img_Url } from "../../constants/constants"
+import { Img_Url,API_KEY } from "../../constants/constants"
 import { useEffect, useState } from "react"
 import axios from "../../axios"
+import YouTube from "react-youtube"
 
 const RowPost = (props) => {
   const [pic,setPic] = useState([])
+  const [url,setUrl] = useState("")
   useEffect(() => {
     axios.get(props.url)
     .then((response)=>{
       console.log(response.data);
       setPic(response.data.results.sort( ()=>Math.random()-0.5 ))
-      //setPic([response.data.results[Math.floor(Math.random()*response.data.results.length)]])
     })
-  }, [props])
-  
+  }, [])
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+  const movieID = (id)=>{
+    console.log(id)
+    axios.get(`movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then((response)=>{
+      if(response.data.results.length!==0){
+        setUrl(response.data.results[0])
+      }else{
+        console.log('Trailer not available');
+      }
+    })  }
   return (
     <div className='row'>
         <div className="heading"> 
@@ -22,9 +39,10 @@ const RowPost = (props) => {
         <div className="posts">
           {pic.map((obj)=>
 
-            <img src={`${pic ? Img_Url+obj.backdrop_path : ""}`} alt="" className={props.small ? "imgSmall" : "img"} />
+            <img onClick={()=>movieID(obj.id)} src={`${pic ? Img_Url+obj.backdrop_path : ""}`} alt="" className={props.small ? "imgSmall" : "img"} />
           )}
         </div>
+        {url && <YouTube videoId={url.key} opts={opts}/>}
     </div>
   )
 }
